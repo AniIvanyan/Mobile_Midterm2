d14d0af16e50b3b0fd9193666a58ff581517860b
Ani Ivanyan <aniivanyan23@gmail.com>
2023-11-15 10:14:43 +0400

    network problem

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,12 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
+    implementation("androidx.lifecycle:lifecycle-livedata:2.4.0-rc01")
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.4.0-rc01")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("androidx.activity:activity-compose:1.3.1")
+    implementation ("androidx.compose.runtime:runtime-livedata:1.1.0")
 }
--- app/src/main/java/com/example/mobile_midterm2/MainActivity.kt
+++ app/src/main/java/com/example/mobile_midterm2/MainActivity.kt
@@ -1,46 +1,137 @@
 package com.example.mobile_midterm2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mobile_midterm2.ui.theme.Mobile_Midterm2Theme
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val viewModel = ViewModelProvider(this)[UserViewModel::class.java]
+
         setContent {
-            Mobile_Midterm2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            AppUI(viewModel = viewModel)
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun AppUI(viewModel: UserViewModel) {
+        val users by viewModel.users.observeAsState(emptyList())
+
+        Column {
+            TopAppBar(
+                title = { Text(text = "User List") }
+            )
+
+            if (users.isNullOrEmpty()) {
+                Text(text = "Loading users...")
+            } else {
+                UserList(users = users)
+            }
+        }
+    }
+}
+
+class UserViewModel : ViewModel() {
+    private val userService = RetrofitClient.userService
+
+    private val _users = MutableLiveData<List<User>>()
+    val users: LiveData<List<User>> get() = _users
+
+    init {
+        viewModelScope.launch {
+            try {
+                val response = userService.getUsers()
+                _users.value = response
+                Log.d("UserViewModel", "Users loaded successfully")
+            } catch (e: Exception) {
+                Log.e("UserViewModel", "Error loading users", e)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UserList(users: List<User>) {
+    LazyColumn {
+        items(users) { user ->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Text(
+                        text = "Name: ${user.name}",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Text(
+                        text = "Email: ${user.email}",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Mobile_Midterm2Theme {
-        Greeting("Android")
+object RetrofitClient {
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com/users/"
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
+
+    val userService: UserService by lazy {
+        retrofit.create(UserService::class.java)
+    }
+}
+
+data class User(
+    val id: Int,
+    val name: String,
+    val username: String,
+    val email: String,
+)
+
+interface UserService {
+    @GET("users")
+    suspend fun getUsers(): List<User>
 }
